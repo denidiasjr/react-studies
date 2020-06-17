67530e2ebe0746228b342c58a496e01ecc0c59ee
Deni Junior <denidiasjr@gmail.com>
2020-06-17 14:12:01 -0300

    Creating getServerSideProps and getting some data

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -1,3 +1,21 @@
-const Country = () => <h1>My country page</h1>
+import { useEffect } from 'react';
+import axios from 'axios';
 
-export default Country;
+const Home = (props) => {
+  return (
+    <h1>My country page</h1>
+  )
+}
+
+export const getServerSideProps = async () => {
+
+  const response = await axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+
+  return {
+    props: {
+      shows: response.data
+    }
+  }
+}
+
+export default Home;
