e8af9c9dc2fca7a7700cf6e095a716ea375a61c8
Deni Junior <denidiasjr@gmail.com>
2020-06-19 09:33:07 -0300

    Creating style with sass

--- tvshow-pro/components/Thumbnail/index.js
+++ tvshow-pro/components/Thumbnail/index.js
@@ -1,11 +1,9 @@
-import ThumbnailStyles from "./styles";
+import './styles.scss';
 
 const Thumbnail = ({ imageUrl, caption }) => (
   <div className="thumbnail">
     <img src={imageUrl} className="thumbnail__image" />
     <h3 className="thumbnail__caption">{caption}</h3>
-
-    <style jsx>{ThumbnailStyles}</style>
   </div>
 )
 
