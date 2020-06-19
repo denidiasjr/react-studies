c675346dbd7334cb63d539bed82fe1cbd1f90052
Deni Junior <denidiasjr@gmail.com>
2020-06-19 09:51:05 -0300

    Creating first link of next

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
 import Thumbnail from '../../components/Thumbnail';
+import Link from 'next/link';
 
 const Home = ({ shows }) => {
 
   const renderShows = () => {
-    return shows.map((showItem, index) => (
-      <li key={index}>
-        <Thumbnail imageUrl={showItem?.show?.image?.medium} caption={showItem?.show?.name} />
-      </li>
-    ))
+    return shows.map((showItem, index) => {
+
+      const { show } = showItem;
+      const defaultImageUrl = 'https://via.placeholder.com/210x295?text=?';
+
+      return (
+        <li key={index}>
+          <Thumbnail imageUrl={show?.image?.medium || defaultImageUrl} caption={show?.name} />
+        </li>
+      )
+    })
   }
 
   return (
     <ul className="tvshow-list">
+      <Link href="/about">
+        About
+      </Link>
       {renderShows()}
     </ul>
   )
