024d518c417d5798955351a69e81e32267d5ed04
Deni Junior <denidiasjr@gmail.com>
2020-06-28 16:25:10 -0300

    Create countries filter on header

--- tvshow-pro/components/Header/index.js
+++ tvshow-pro/components/Header/index.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import './styles.scss';
 
+const countries = [
+  {
+    label: 'us',
+    name: 'United States'
+  },
+  {
+    label: 'br',
+    name: 'Brazil'
+  }
+]
+
 const Header = () => {
+
+  const router = useRouter();
+
+  const [selectedCountry, setSelectedCountry] = useState(router?.query?.country);
+
+  const renderCountriesOptions = options => {
+    return options.map(option => {
+      return (
+        <option key={option.label} value={option.label}>{option.name}</option>
+      )
+    })
+  }
+
+  const handleChange = event => {
+    setSelectedCountry(event?.target?.value);
+  }
+
   return (
     <div className="header">
-      Header
+      <select className="header__filter" value={selectedCountry} onChange={handleChange}>
+        {renderCountriesOptions(countries)}
+      </select>
     </div>
   )
 }
