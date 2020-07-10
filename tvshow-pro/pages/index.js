9ce2f1a2380f13ca61b5744508f14d87ea868238
Deni Junior <denidiasjr@gmail.com>
2020-07-10 18:50:10 -0300

    Redirecting from home to country page

--- tvshow-pro/pages/index.js
+++ tvshow-pro/pages/index.js
@@ -1,9 +1,16 @@
-const Home = () => {
-  return (
-    <div>
-      This is my homepage uhuuul!
-    </div>
-  )
+import Router from 'next/router';
+
+const Home = () => null
+
+export const getServerSideProps = async context => {
+
+  const country = context?.query?.country || 'us';
+
+  process.browser ?
+    Router.replace('/[country]', `${country}`) :
+    context.res.writeHead(302, { Location: `/${country}` });
+
+  context.res.end();
 }
 
 export default Home;
