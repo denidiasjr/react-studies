4d35dd46e451cb7a041d6226386081cf20e56f35
Deni Junior <denidiasjr@gmail.com>
2020-10-20 22:17:34 -0300

    Create tests for new GuessedWords section

--- react-testing-jest-course/jotto/src/components/GuessedWords/GuessedWords.js
+++ react-testing-jest-course/jotto/src/components/GuessedWords/GuessedWords.js
@@ -11,11 +11,36 @@ const GuessedWords = (props) => {
     <span data-test="guess-instructions">
       Try to guess the secret word!
     </span>
+  );
+
+  const guessedWordsContent = guessedWords.length > 0 && (
+    <div data-test="guessed-words">
+      <h3>Guessed Words</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Guess</th>
+            <th>Matching Letters</th>
+          </tr>
+        </thead>
+        <tbody>
+          { 
+            guessedWords.map((guess, index) => (
+              <tr data-test="guessed-word" key={index}>
+                <td>{guess.guessedWord}</td>
+                <td>{guess.letterMatchCount}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
   )
 
   return (
     <div data-test="component-guessed-words">
       {guessInstructions}
+      {guessedWordsContent}
     </div>
   )
 }
