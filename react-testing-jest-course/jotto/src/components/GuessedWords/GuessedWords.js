8858cc8a46b0ab88488627d5b6ff94c02121536a
Deni Junior <denidiasjr@gmail.com>
2020-10-20 22:04:54 -0300

    Creating GuessedWords components and tests

---
+++ react-testing-jest-course/jotto/src/components/GuessedWords/GuessedWords.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = (props) => {
+
+  const {
+    guessedWords = []
+  } = props;
+
+  const guessInstructions = guessedWords.length === 0 && (
+    <span data-test="guess-instructions">
+      Try to guess the secret word!
+    </span>
+  )
+
+  return (
+    <div data-test="component-guessed-words">
+      {guessInstructions}
+    </div>
+  )
+}
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired
+}
+
+export default GuessedWords;
