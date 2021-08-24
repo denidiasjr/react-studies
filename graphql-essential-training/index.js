8bf4bc1bbb4766d4bd296323e654e9fb154d53f5
Deni Junior <denidiasjr@gmail.com>
2021-08-24 10:58:50 -0300

    First steps with GraphQL

--- graphql-essential-training/index.js
+++ graphql-essential-training/index.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import schema from './schema'
+import { graphqlHTTP } from 'express-graphql'
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Learning GraphQL here on LinkedIn')
 })
+
+const root = { hello: () => "Hi, my name is Deni!"}
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true 
+}))
 
 app.listen(8080, console.log('Listening to 8080 port'))
