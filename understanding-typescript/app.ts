902e67672141b49461596f65c07ab7226966bdd8
Deni Junior <denidiasjr@gmail.com>
2021-09-12 16:08:00 -0300

    Testing tsconfig,json and debug

--- understanding-typescript/app.ts
+++
@@ -1,5 +0,0 @@
-function throwError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-  // while (true) {}
-}
-console.log(throwError('Service unavailable', 500));
